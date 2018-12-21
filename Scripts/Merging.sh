0130ba21496ee24baa2edd73ab7635312c40c3eb
Davide Bolognini <39052119+davidebolo1993@users.noreply.github.com>
2018-12-21 11:09:47 +0100

    Create Merging.sh

---
+++ Scripts/Merging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#merging .bam files without exeeding limit of 1024 open files at time
+
+file=$(ls $1'*.srt.bam' | sort -R | tail -1)
+samtools view -bH $file > $2.bam
+find $1 -name '*srt.bam' -exec samtools view -b {} \; >> $2'.bam'
+samtools sort $2'.bam' > $2'.srt.bam'
+rm $2'.bam'
+samtools index $2'srt.bam'
