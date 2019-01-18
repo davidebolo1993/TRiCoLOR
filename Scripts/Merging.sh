e4d24b076ffea19cde73c938cab88a5bd07c6e33
Davide Bolognini <39052119+davidebolo1993@users.noreply.github.com>
2019-01-18 11:05:56 +0100

    Update Merging.sh

--- Scripts/Merging.sh
+++ Scripts/Merging.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#merging .bam files without exceeding the number of opened files at the same time
+#merging .bam files without exeeding limit of 1024 open files at time
 
 cd $2
-samtools view -H $1 > $3.sam
-find . -type f -name '*srt.bam' -exec samtools view {} \; >> $3'.sam'
-htsbox samview -bS $3'.sam' > $3'.bam' && rm $3'.sam'
-samtools sort $3'.bam' > $3'.srt.bam'
-rm $3'.bam' && samtools index $3'.srt.bam'
-find . -type f -not -name '*.merged.srt.bam' -not -name '*merged.srt.bam.bai' -not -name '*.tsv' -delete
+
+samtools view -H $1 > $3'.merged.sam'
+find . -type f -name '*.srt.bam' -not -name 'chr*' -exec samtools view -b {} \; >> $3'.merged.sam'
+htsbox samview -bS $3'.merged.sam' > $3'.merged.bam' && rm $3'.merged.sam'
+samtools sort $3'.merged.bam' > $3'.merged.srt.bam'
+rm $3'.merged.bam' && samtools index $3'.merged.srt.bam'
+find . -type f -not -name '*.merged.srt.bam' -not -name '*.merged.srt.bam.bai' -not -name '*.tsv' -delete
