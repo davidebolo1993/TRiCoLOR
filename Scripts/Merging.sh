9d73280f960adbcbc2efa29d736c77771bb493f9
Davide Bolognini <davidebolognini7@gmail.com>
2019-01-29 11:02:55 +0100

    Update Merging.sh

--- Scripts/Merging.sh
+++ Scripts/Merging.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#merging .bam files without exeeding limit of 1024 open files at the same time
+#merging .bam files without exeeding limit of 1024 open files at time
 
 cd $2
 
 samtools view -H $1 > $3'.merged.sam'
-find . -type f -name '*.srt.bam' -not -name 'chr*' -exec samtools view -b {} \; >> $3'.merged.sam'
+find . -type f -name '*.srt.bam' -not -name 'chr*' -exec samtools view {} \; >> $3'.merged.sam'
 htsbox samview -bS $3'.merged.sam' > $3'.merged.bam' && rm $3'.merged.sam'
 samtools sort $3'.merged.bam' > $3'.merged.srt.bam'
 rm $3'.merged.bam' && samtools index $3'.merged.srt.bam'
-find . -type f -not -name '*.merged.srt.bam' -not -name '*.merged.srt.bam.bai' -not -name '*.tsv' -delete
+find . -type f -not -name '*.merged.srt.bam' -not -name '*.merged.srt.bam.bai' -not -name '*.bed' -delete
