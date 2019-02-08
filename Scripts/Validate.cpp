a75bc03d85d34b8f894cb466f57d3e269e2ad58d
Davide Bolognini <davidebolognini7@gmail.com>
2019-02-08 13:48:42 +0100

    Update Validate.cpp

--- Scripts/Validate.cpp
+++ Scripts/Validate.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
 
 
 int main(int argc, char* argv[])
 {
   //do something here
+  string a = boost::filesystem::canonical('/home/bolognin/').string();
+  std::cout << a << std::endl;
+  
   return 0;
 }
