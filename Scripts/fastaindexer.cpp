95fabbeb96f23d7947a97b5bbcd1a8af22988603
Davide Bolognini <davidebolognini7@gmail.com>
2019-02-18 16:09:29 +0100

    Update fastaindexer.cpp

--- Scripts/fastaindexer.cpp
+++ Scripts/fastaindexer.cpp
@@ -5,6 +5,14 @@
 #include <sdsl/suffix_arrays.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+
+
+
 
 using namespace sdsl;
 
@@ -21,73 +29,137 @@ int main(int argc, char **argv) {
 
     boost::posix_time::ptime timer;
 
-    std::ifstream input(reference.string());
+    std::ifstream input(reference.string(), std::ios_base::in | std::ios_base::binary);
 
     if (input.is_open()) {
 
+        char fcode[4];
+        input.seekg(0);
+        input.read(fcode, 4);
 
-        timer = boost::posix_time::second_clock::local_time();
-        std::cout << '[' << boost::posix_time::to_simple_string(timer) << "] " << "Modifying .fasta before indexing ..." << std::endl;
-
-   
-        std::string line;
-        std::ofstream output;
-
-        output.open(modreference.string());
-
-        bool first_header=true;
-
-        while (std::getline(input, line)) {
-
-            if(line.empty()) { // exclude blank lines
-
-                continue;
-
-            }
+    // first, check if the input is gzipped
 
 
-            else if (line[0] == '>') {
+        if (((uint8_t)fcode[0] == (uint8_t)0x1f) && ((uint8_t)fcode[1] == (uint8_t)0x8b)) { //input is gzipped
 
+            timer = boost::posix_time::second_clock::local_time();
+            std::cout << '[' << boost::posix_time::to_simple_string(timer) << "] " << "Preparing for FM indexing, gzipped input ..." << std::endl;
 
-                if (!first_header) {
-                
-                    output << std::endl;
-                
-                }
+            std::ifstream to_stream(reference.string(), std::ios_base::in | std::ios_base::binary);
 
-                else {
+            boost::iostreams::filtering_streambuf<boost::iostreams::input> inbuf;
+            inbuf.push(boost::iostreams::gzip_decompressor());
+            inbuf.push(to_stream);
+            //Convert streambuf to istream
+            std::istream instream(&inbuf);
+            //Iterate lines
+            std::string line;
+            std::ofstream output(modreference.string());
 
-                    first_header=false;
+            bool first_header=true;
+
+            while (std::getline(instream, line)) {
+
+                if(line.empty()) { // exclude blank lines
 
                     continue;
 
-
                 }
 
+                else if (line[0] == '>') {
+
+
+                    if (!first_header) {
+                    
+                        output << std::endl;
+                    
+                    }
+
+                    else {
+
+                        first_header=false;
+                        continue;
+
+                    }
+
+
+                }
+           
+                else {
+               
+                    output << boost::to_upper_copy(line);
+           
+                }
 
             }
-       
-            else {
-           
-                output << boost::to_upper_copy(line);
-       
-            }
+
+            output << std::endl;
+            output.close();
+            to_stream.close();
 
         }
 
 
-        output << '\n';
-        output.close();
-        input.close();
+        else {
+
+
+            timer = boost::posix_time::second_clock::local_time();
+            std::cout << '[' << boost::posix_time::to_simple_string(timer) << "] " << "Preparing for FM indexing, not gizzipped input ..." << std::endl;
+           
+            std::ifstream to_stream(reference.string(), std::ios_base::in | std::ios_base::binary);
+            std::string line;
+            std::ofstream output(modreference.string());
+
+            bool first_header=true;
+
+            while (std::getline(to_stream, line)) {
+
+                if(line.empty()) { // exclude blank lines
+
+                    continue;
+
+                }
+
+                else if (line[0] == '>') {
+
+
+                    if (!first_header) {
+                    
+                        output << std::endl;
+                    
+                    }
+
+                    else {
+
+                        first_header=false;
+
+                        continue;
+
+
+                    }
+
+
+                }
+           
+                else {
+               
+                    output << boost::to_upper_copy(line);
+           
+                }
+
+            }
+
+            output << std::endl;
+            output.close();
+            to_stream.close();
+
+        }
+
 
         timer = boost::posix_time::second_clock::local_time();
-        std::cout << '[' << boost::posix_time::to_simple_string(timer) << "] " << "FM-indexing ..." << std::endl;
-
-
+        std::cout << '[' << boost::posix_time::to_simple_string(timer) << "] " << "Starting FM-indexing ..." << std::endl;
         construct(fm_index,modreference.string(), 1);
         store_to_file(fm_index,fm.string());
-
-
         timer = boost::posix_time::second_clock::local_time();
         std::cout << '[' << boost::posix_time::to_simple_string(timer) << "] " << "Done" << std::endl;
 
@@ -99,9 +171,8 @@ int main(int argc, char **argv) {
 
     else {
 
+        return 1;
 
-        return -1;
     }
-
 
 }
