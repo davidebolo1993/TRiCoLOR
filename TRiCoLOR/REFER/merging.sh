25793b152ba4744d77a808f510691343547cd569
Davide Bolognini <davidebolognini7@gmail.com>
2019-03-25 16:19:52 +0100

    Add files via upload

---
+++ TRiCoLOR/REFER/merging.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#merging .bam files without exeeding limit of 1024 open files at time
+
+cd $2
+
+samtools view -H $1 > $3'.merged.sam'
+find . -type f -name '*.srt.bam' -not -name 'chr*' -exec samtools view {} \; >> $3'.merged.sam'
+samtools view -bS $3'.merged.sam' > $3'.merged.bam' && rm $3'.merged.sam'
+samtools sort $3'.merged.bam' > $3'.merged.srt.bam'
+rm $3'.merged.bam' && samtools index $3'.merged.srt.bam'
+find . -type f -not -name '*.merged.srt.bam' -not -name '*.merged.srt.bam.bai' -not -name '*.bed' -delete
+
