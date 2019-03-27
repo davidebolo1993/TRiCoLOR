f879c7ba92bfb875fe904babfdcc69b97b68c1a0
Davide Bolognini <davidebolognini7@gmail.com>
2019-03-27 12:51:38 +0100

    Update parser.py

--- TRiCoLOR/REFER/Helper/parser.py
+++ TRiCoLOR/REFER/Helper/parser.py
@@ -7,7 +7,6 @@ import os
 import glob
 import math
 import subprocess
-from bisect import bisect_left
 
 
 # additional libraries
@@ -16,110 +15,22 @@ import pysam
 
 
 
-def Sub_None(list_of_coord):
+def sub_none(list_of_coord):
 
 	return [-999999 if v is None else v for v in list_of_coord] #return a very small number so that it won't be take into account
-
-
-def Start_Index(list_of_coord,start):
-
-	if start in list_of_coord:
-
-		index=bisect_left(list_of_coord, start)
-		return index
-
-	else:
-
-		if any(x < start and x != -999999 for x in list_of_coord): #if there is something lower than start, take closest
-
-			alternative=min(list_of_coord, key=lambda x:abs(x-start))
-			index= bisect_left(list_of_coord, alternative)
-			return index
-
-		else:
-
-			return #no start coordinate
-
-
-def End_Index(list_of_coord,end):
-
-	if end in list_of_coord:
-
-		index=bisect_left(list_of_coord, end)
-		return index
-
-	else:
-
-		if any(x > end for x in list_of_coord): #check if any coord is higher in read than end, else return None (sequence has not an end in interval)
-
-			alternative = min(list_of_coord, key=lambda x:abs(x-end))
-			index=bisect_left(list_of_coord, alternative)
-
-		else:
-
-			return #no overlapping end
-
-
-def Basic_Infos(pysam_AlignedSegment):
-
-	identifier=pysam_AlignedSegment.query_name	
-	seq=pysam_AlignedSegment.seq #give the correct result even for reverse strand, DO NOT REVERSE-COMPLEMENT
-	
-	return (identifier,seq)
-
-
-def Get_Coords(pysam_AlignedSegment,start,end):
-
-
-	a_coord=pysam_AlignedSegment.get_reference_positions(full_length=True) #list that indicates the reference position on which each base alignes (None included for insertion/soft clipped bases)
-	identifier=pysam_AlignedSegment.query_name
-	
-	a_coord=Sub_None(a_coord)	
-
-	ind_start=Start_Index(a_coord,start)
-	ind_end=End_Index(a_coord,end)
-
-	if ind_start is None or ind_end is None:
-
-		return
-
-	elif ind_end - ind_start <= 10: #exclude sequences for which start-end are very close (sequences too short)
-
-		return
-
-	else:
-		
-		return (identifier,ind_start,ind_end)
-
-
-def split_equal(value, parts):
-
-	value = float(value)
-	
-	return int(value/parts), len([i*value/parts for i in range(1,parts+1)])
-
-
-def SizeChecker(start, end, treshold=2): #adjust size of interval so that we don't have sequences too short and they can me mapped with more accuracy
-
-	if end-start <= treshold*1000:
-
-		return start, end, end-start
-
-	else:
-
-		size, len_=split_equal(end-start, math.ceil((end-start)/(treshold*1000)))
-
-		return start, start+(size*len_), size 
-	
 
 
 def check_coverage(pysam_AlignmentFile, chromosome, start, end, coverage): #check if we have at least the wanted coverage for the interval
 
 	counter = 0
 
-	for reads in pysam_AlignmentFile.fetch(chromosome, start, end):
+	for read in pysam_AlignmentFile.fetch(chromosome, start, end):
+		
+		if not read.is_unmapped and not read.is_secondary and not read.is_supplementary:
 
-		counter +=1
+			if read.reference_start <= start and read.reference_end >= end: #make sure that the same read cover the entire region
+
+				counter +=1
 
 	if counter >= coverage:
 
@@ -130,118 +41,87 @@ def check_coverage(pysam_AlignmentFile, chromosome, start, end, coverage): #chec
 		return False
 
 
-def Bamfile_Analyzer(bamfilein,chromosome,start,end, coverage): #double sliding window function to retrive informations
+def fastagen(header,sequence,iteration,out):
+
+
+	filename=os.path.abspath(out+'/Fasta'+str(iteration+1)+'.unaligned.fa')
+
+	with open(os.path.abspath(out+'/Fasta'+str(iteration+1)+'.unaligned.fa'), 'a') as fastaout:
+
+		fastaout.write('>' + header + '\n' + sequence + '\n')
+
+
+
+def split_equal(value, parts):
+
+	value = float(value)
+	
+	return int(value/parts), len([i*value/parts for i in range(1,parts+1)])
+
+
+def sizechecker(start, end): #adjust size of interval so that we don't have sequences too short and they can me mapped with more accuracy
+
+	if end-start <= 2000:
+
+		return start, end, end-start
+
+	else:
+
+		size, len_=split_equal(end-start, math.ceil((end-start)/2000))
+
+		return start, start+(size*len_), size 
+
+
+def Bamfile_Analyzer(bamfilein,chromosome,start,end, coverage, fastaout):
 
 	bamfile=pysam.AlignmentFile(bamfilein,'rb')
-	start,end,size=SizeChecker(start,end) #split the period in sub-period of max length 2000 bp
+	
+	start,end,size=sizechecker(start,end) #split the period in sub-period of equal length
 
-	i=0
+	next_=start+size
 
-	pointer=start+1 #slicing window
+	iteration=0
 
-	inf=[] #list for appending results
-	coord=[] #list for appending coordinates
+	final=False
 
-	while end-1 >= start:
+	while not final:
 
-		inf.append([])
-		coord.append([])
+		if check_coverage(bamfile, chromosome, start, next_, coverage): # coverage check for the region
 
-		if check_coverage(bamfile, chromosome, start, pointer, coverage):
+			for read in bamfile.fetch(chromosome,start,next_):
 
-			for read in bamfile.fetch(chromosome,start,pointer):
+				if not read.is_unmapped and not read.is_secondary and not read.is_supplementary: #skip everything but primary, as in coverage check
 
-				if not read.is_unmapped and not read.is_secondary and not read.is_supplementary:
+					read_start=read.reference_start
+					read_end=read.reference_end
 
-					inf[i].append(Basic_Infos(read))
+					if read_start <= start and read_end >= read_end: #make sure that the same read cover the entire region
 
-					if end > start+size:
+						sequence=read.seq
+						coord=Sub_None(read.get_reference_positions(full_length=True))
+						header=read.query_name
 
-						coord[i].append(Get_Coords(read,start,start+size))
+						s_,e_=min(coord, key=lambda x:abs(x-start)), min(coord, key=lambda x:abs(x-(start+size))) #must exist
+						s_i,e_i = [i for i,e in enumerate(coord) if e == s_][0], [i for i,e in enumerate(coord) if e == e_][0] #must exist
 
-					else:
+						fastagen(header,sequence[s_i:e_i+1],iteration,fastaout)
 
-						coord[i].append(Get_Coords(read,start,end))
 
-		if end-start > size:
+		iteration+=1
+
+		if end-next_ >= size:
 
 			start += size
-			pointer += size
+			next_ += size
 
-		elif end-start <= size and start < end-1:
+		elif end-next_ <  size:
 
-			start = end-1
-			pointer = end
+			final=True
 
-		else: 
-
-			break
-
-		i+=1 #update counter in order to store results progressively
-
+	
 	bamfile.close()
 
-	return inf,coord[:len(coord)-1] 
 
-
-def Reorder(l1,l2): #reorder and filtering
-
-	l2_2=list(filter(None.__ne__,l2)) #filtering None for reads that do not have end in interval
-	l2_el=[el[0] for el in l2_2]
-	mapping = dict((x[0], x[1:]) for x in l1)
-	l1[:] = [(x,) + mapping[x] for x in l2_el]
-
-	return l1,l2_2
-
-
-def InCommon(info_list,coord_list):
-
-	filtered_seq=[]
-	filtered_coord=[]
-
-	for i in range(len(info_list)-1):
-
-		fip=info_list[i]
-		sip=info_list[i+1]
-
-
-		if fip != [] and sip != []:
-
-			id_fip=[a for a,b in fip]
-			id_sip=[a for a,b in sip]
-			int_id=list(set(id_fip).intersection(id_sip))
-			com_seq=[(a,b) for a,b in fip if a in int_id]
-			l1,l2=Reorder(com_seq,coord_list[i])
-			filtered_seq.append(l1)
-			filtered_coord.append(l2)
-
-		else:
-
-			continue
-
-	return [el for el in filtered_seq if el != []],[el for el in filtered_coord if el !=[]]
-
-
-def Fasta_Generator(filtered_seq,filtered_coord,out):
-
-	for i in range(len(filtered_seq)):
-
-		dirname=os.path.abspath(out)
-
-		if not os.path.exists(dirname):
-
-			os.makedirs(dirname)
-
-		fname=os.path.abspath(dirname+'/Fasta'+str(i+1)+'.unaligned.fa')
-
-		f=open(fname,'a')
-
-		for j in range(len(filtered_seq[i])):
-
-			fasta_in_window='>'+filtered_seq[i][j][0]+'\n'+filtered_seq[i][j][1][filtered_coord[i][j][1]:filtered_coord[i][j][2]+1]+'\n' #get sequence from start to end (end included)
-			f.write(fasta_in_window)
-
-		f.close() 
 
 
 
