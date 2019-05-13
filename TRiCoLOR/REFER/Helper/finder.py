6d584fa30a74bf2bae8ec0d58f970808581a808e
Davide Bolognini <davidebolognini7@gmail.com>
2019-05-13 15:53:11 +0200

    Update finder.py

--- TRiCoLOR/REFER/Helper/finder.py
+++ TRiCoLOR/REFER/Helper/finder.py
@@ -18,323 +18,336 @@ import editdistance
 
 def nestover(SortedIntervals, string, coords):
 
-    extended=[]
+	extended=[]
 
-    i=0
+	i=0
 
-    while i < len(SortedIntervals):
+	while i < len(SortedIntervals):
 
-        if extended==[]:
+		if extended==[]:
 
-            s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
-            string_s,string_e = bisect_left(coords,s_), bisect_right(coords, e_)
-            num=string[string_s:string_e].count(SortedIntervals[i][0])
-            extended.append((SortedIntervals[i][0], s_, e_, num))
-        
+			s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
+			string_s,string_e = bisect_left(coords,s_), bisect_right(coords, e_)
+			num=string[string_s:string_e].count(SortedIntervals[i][0])
+			extended.append((SortedIntervals[i][0], s_, e_, num))
+		
 
-        else:
+		else:
 
-            c1=extended[-1][3]
+			if extended[-1][2] >= SortedIntervals[i][1] and extended[-1][2] < SortedIntervals[i][2]: #the two intervals overlap in end(1)/start(2)
 
-            if extended[-1][2] >= SortedIntervals[i][1]: #the two intervals overlap
+				if abs(extended[-1][2]-SortedIntervals[i][1]) >= round((extended[-1][2]-extended[-1][1])/2): #big overlap, check if we want to keep both or just one
 
-                lenprevious=extended[-1][2]-extended[-1][1] #length previous interval
+					#count for the old interval
+					c1=extended[-1][2]
 
-                if extended[-1][2] - SortedIntervals[i][1] >= round(lenprevious/2): #big overlap, check if we want to keep both or just one
+					#count for the new interval
+					s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
+					ss__, se__=bisect_left(coords,s_), bisect_right(coords, e_)
+					m2=SortedIntervals[i][0]
+					c2=string[ss__:se__].count(SortedIntervals[i][0])
 
-                    m1=extended[-1][0]
-                    m2=SortedIntervals[i][0]
+					#motifs
+					m1=extended[-1][0]
 
-                    new_s,new_e=min(extended[-1][1],SortedIntervals[i][1]), max(extended[-1][2],SortedIntervals[i][2])
-                    string_s,string_e = bisect_left(coords,new_s), bisect_right(coords, new_e)
-                    st_=string[string_s:string_e]
-                    
-                    rank1,count1=Markovchain(m1,st_),st_.count(m1)
-                    rank2,count2=Markovchain(m2,st_),st_.count(m2)
+					#extend and rank
+					new_s,new_e=min(extended[-1][1],SortedIntervals[i][1]), max(extended[-1][2],SortedIntervals[i][2])
+					string_s,string_e = bisect_left(coords,new_s), bisect_right(coords, new_e)
+					st_=string[string_s:string_e]
+					rank1,count1=Markovchain(m1,st_),st_.count(m1)
+					rank2,count2=Markovchain(m2,st_),st_.count(m2)
 
+					if rank2 > rank1: #rank2 higher
 
-                    if rank2 > rank1: #rank2 higher
+						if count2 > c2:
 
-                        extended.remove(extended[-1])
-                        extended.append((m2,SortedIntervals[i][1], SortedIntervals[i][2], count2))
+							extended.remove(extended[-1])
+							extended.append((m2,new_s, new_e, count2))
 
+						else: #extending did not increase the number of repetitions, keep original
 
-                    elif rank2 == rank1: #two ranks are the same.
-
-                        if m2 in m1*2: #if rep in second interval is the same of rep in first interval but simply rotated keep interval with more reps
-
-                            if count2 > count1:
-
-                                extended.remove(extended[-1])
-                                extended.append((m2,SortedIntervals[i][1], SortedIntervals[i][2], count2))
-
-                            elif count2 < count1:
-
-                                if c1==count1: #extending does not increment count, keep original:
-
-                                    pass
-
-                                else: #extending can lead only to increase, not deacrease:
-
-                                    extended.remove(extended[-1])
-                                    extended.append((m1,new_s,new_e,count1))
-
-                            else: #counts are equal, try to discriminate on region length;
+							extended.remove(extended[-1])
+							extended.append((m2,s_,e_, c2))
 
 
-                                if extended[-1][2]-extended[-1][1] >= SortedIntervals[i][2]-SortedIntervals[i][1]:
+					elif rank2 < rank1: #rank1 higher
 
-                                    pass
+						if count1 > c1:
 
-                                else:
+							extended.remove(extended[-1])
+							extended.append((m1,new_s, new_e, count1))
 
-                                    extended.remove(extended[-1])
-                                    extended.append((m2,SortedIntervals[i][1], SortedIntervals[i][2], count2))
+						else: #extending did not increase the number of repetitions, keep original
 
-                        else: #different reps with same probability, keep both
-
-                            extended.append((m2,new_s, new_e, count2))
+							pass
 
 
-                    else: #rank1 higher
+					else: #two ranks are the same
 
-                        pass
+						if m2 in m1*2: #if rep in second interval is the same of rep in first interval but simply rotated keep interval with higher count
 
-                else: #overlapping region is shorter than treshold. Keep both reps, they will be merged in VCF but motifs will be kept separated
+							if count2 > count1:
 
-                    s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
-                    string_s,string_e = bisect_left(coords,s_), bisect_right(coords, e_)
-                    num = string[string_s:string_e].count(SortedIntervals[i][0])
-                    extended.append((SortedIntervals[i][0], s_, e_, num))
-                    
-            else:
+								if count2 > c2:
 
-                if extended[-1][1] <=  SortedIntervals[i][1] and extended[-1][2] >= SortedIntervals[i][2]: #following interval is smaller than previous
+									extended.remove(extended[-1])
+									extended.append((m2,new_s, new_e, count2))
 
-                    pass
+								else:
 
-                elif extended[-1][2] < SortedIntervals[i][1]: #following does not overlap and is not nested
-
-                    s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
-                    string_s,string_e = bisect_left(coords,s_), bisect_right(coords, e_)
-                    num = string[string_s:string_e].count(SortedIntervals[i][0])
-                    extended.append((SortedIntervals[i][0], s_, e_, num))
-
-        i+=1
+									extended.remove(extended[-1])
+									extended.append((m2,s_,e_, count2))
 
 
-    return extended
+							else:
+
+								if count1 > c1:
+
+									extended.remove(extended[-1])
+									extended.append((m1,new_s,new_e,count1))
+
+								else:
+
+									pass
+
+						else: #different reps with same probability, keep previous and new but separate. They will be merged in VCF but motifs will be kept separated
+
+							extended.append((m2,s_, e_, c2))
+
+				else: #overlapping region is shorter than half the length of previous. Keep both reps, they will be merged in VCF but motifs will be kept separated
+
+					s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
+					string_s,string_e = bisect_left(coords,s_), bisect_right(coords, e_)
+					num = string[string_s:string_e].count(SortedIntervals[i][0])
+					extended.append((SortedIntervals[i][0], s_, e_, num))
+					
+			
+			elif extended[-1][1] <=  SortedIntervals[i][1] and extended[-1][2] >= SortedIntervals[i][2]:
+
+				pass
+			
+			elif extended[-1][2] < SortedIntervals[i][1]: #following does not overlap and is not nested
+
+				s_,e_=SortedIntervals[i][1], SortedIntervals[i][2]
+				string_s,string_e = bisect_left(coords,s_), bisect_right(coords, e_)
+				num = string[string_s:string_e].count(SortedIntervals[i][0])
+				extended.append((SortedIntervals[i][0], s_, e_, num))
+
+		i+=1
+
+
+	return extended
 
 
 
 def RepeatsFinder(string,kmer,times, maxkmerlength, overlapping): 
 
-    seen=set()
+	seen=set()
 
-    if kmer != 0 and times == 0:
+	if kmer != 0 and times == 0:
 
-        my_regex = r'(.{'  + str(kmer) + r',})\1+' if not overlapping else r'(?=(.{'  + str(kmer) + r',})\1+)'
+		my_regex = r'(.{'  + str(kmer) + r',})\1+' if not overlapping else r'(?=(.{'  + str(kmer) + r',})\1+)'
 
-    elif kmer == 0 and times != 0:
+	elif kmer == 0 and times != 0:
 
-        my_regex = r'(.+?)\1{' + str(times-1) + r',}' if not overlapping else r'(?=(.+?)\1{' + str(times-1) + r',})'
+		my_regex = r'(.+?)\1{' + str(times-1) + r',}' if not overlapping else r'(?=(.+?)\1{' + str(times-1) + r',})'
 
-    elif kmer != 0 and times != 0:
+	elif kmer != 0 and times != 0:
 
-        my_regex = r'(.{'  + str(kmer) + r',})\1{' + str(times-1) + r',}' if not overlapping else r'(?=(.{'  + str(kmer) + r',})\1{' + str(times-1) + r',})'
+		my_regex = r'(.{'  + str(kmer) + r',})\1{' + str(times-1) + r',}' if not overlapping else r'(?=(.{'  + str(kmer) + r',})\1{' + str(times-1) + r',})'
 
-    else:
+	else:
 
-        my_regex = r'(.+?)\1+' if not overlapping else r'(?=(.+?)\1+)'
+		my_regex = r'(.+?)\1+' if not overlapping else r'(?=(.+?)\1+)'
 
-    r=re.compile(my_regex)
+	r=re.compile(my_regex)
 
-    for match in r.finditer(string):
+	for match in r.finditer(string):
 
-        motif=match.group(1)
+		motif=match.group(1)
 
-        if motif not in seen and len(motif) <= maxkmerlength:
+		if motif not in seen and len(motif) <= maxkmerlength:
 
-            seen.add(motif)
+			seen.add(motif)
 
-    return seen
+	return seen
 
 
 def Get_Alignment_Positions(bamfilein): #as the consensus sequence is supposed to generate just one sequence aligned to the reference, secondary alignments are removed
   
-    coords=[]
-    seq=[]
+	coords=[]
+	seq=[]
 
-    bamfile=pysam.AlignmentFile(bamfilein,'rb')
+	bamfile=pysam.AlignmentFile(bamfilein,'rb')
 
-    for read in bamfile.fetch():
+	for read in bamfile.fetch():
 
-        if not read.is_unmapped and not read.is_secondary and not read.is_supplementary:
+		if not read.is_unmapped and not read.is_secondary and not read.is_supplementary:
 
-            coords = read.get_reference_positions(full_length=True)
-            seq=read.seq
+			coords = read.get_reference_positions(full_length=True)
+			seq=read.seq
 
-        else:
+		else:
 
-            bamfile.close()
-            return coords,seq
+			bamfile.close()
+			return coords,seq
 
-    bamfile.close()
+	bamfile.close()
 
-    return coords,seq
+	return coords,seq
 
 
 def modifier(coordinates): #fast way to remove None and substitute with closest number in list
 
-    
-    coordinates=[el+1 if el is not None else el for el in coordinates] #get true coordinates
-    start=next(ele for ele in coordinates if ele is not None)
+	
+	coordinates=[el+1 if el is not None else el for el in coordinates] #get true coordinates
+	start=next(ele for ele in coordinates if ele is not None)
 
-    for ind, ele in enumerate(coordinates):
-        
-        if ele is None:
+	for ind, ele in enumerate(coordinates):
+		
+		if ele is None:
 
-            coordinates[ind] = start
-        
-        else:
+			coordinates[ind] = start
+		
+		else:
 
-            start = ele
+			start = ele
 
-    return coordinates
+	return coordinates
 
 
 
 def Get_Alignment_Coordinates(coord_list,repetitions):
 
-    rep_coord_list=[(a,b,c) for a,b,c in zip([repetition[0] for repetition in repetitions],[coord_list[repetition[1]] for repetition in repetitions],[coord_list[repetition[2]] for repetition in repetitions])]
+	rep_coord_list=[(a,b,c) for a,b,c in zip([repetition[0] for repetition in repetitions],[coord_list[repetition[1]] for repetition in repetitions],[coord_list[repetition[2]] for repetition in repetitions])]
 
-    return rep_coord_list
+	return rep_coord_list
 
 
 def look_for_self(rep,sequence): #build another regular expression, looking for the core of the repetition
 
-    for match in re.finditer(rep,sequence):
+	for match in re.finditer(rep,sequence):
 
-        yield (match.group(), match.start(), match.start()+len(match.group())*int(len(match.group())/len(match.group()))-1,int(len(match.group())/len(match.group())))
+		yield (match.group(), match.start(), match.start()+len(match.group())*int(len(match.group())/len(match.group()))-1,int(len(match.group())/len(match.group())))
 
 
 def dfs(adj_list, visited, vertex, result, key):
 
-    visited.add(vertex)
-    result[key].append(vertex)
+	visited.add(vertex)
+	result[key].append(vertex)
 
-    for neighbor in adj_list[vertex]:
+	for neighbor in adj_list[vertex]:
 
-        if neighbor not in visited:
+		if neighbor not in visited:
 
-            dfs(adj_list, visited, neighbor, result, key)
+			dfs(adj_list, visited, neighbor, result, key)
 
 
 def check_edit(string1, string2, allowed): #cython-based way to check for edit distance, faster than check every time for neighbors
 
-    if len(string2) <= allowed: #string 2 is empty or is motif of length leq allowed
+	if len(string2) <= allowed: #string 2 is empty or is motif of length leq allowed
 
-        return True
+		return True
 
-    elif editdistance.eval(string1,string2) <= allowed:
+	elif editdistance.eval(string1,string2) <= allowed:
 
-        return True
+		return True
 
-    return False
+	return False
 
 
 
 def Markovchain(motif,string):
 
-    STATE_LEN = len(motif)
+	STATE_LEN = len(motif)
 
-    model = defaultdict(Counter)
+	model = defaultdict(Counter)
 
-    for i in range(len(string) - STATE_LEN):
-        state = string[i:i + STATE_LEN]
-        next_ = string[i + STATE_LEN :i + STATE_LEN*2]
-        model[state][next_] += 1
+	for i in range(len(string) - STATE_LEN):
+		state = string[i:i + STATE_LEN]
+		next_ = string[i + STATE_LEN :i + STATE_LEN*2]
+		model[state][next_] += 1
 
 
-    motif_probability=(model[motif][motif]/len(string))*len(motif) + len(motif)/len(string) #reward based on motif length
+	motif_probability=(model[motif][motif]/len(string))*len(motif) + len(motif)/len(string) #reward based on motif length
 
-    return motif_probability
+	return motif_probability
 
 
 
 def check_ref(string1, string2): #check if ref_string and test_string are different
 
-    return string1 != string2
+	return string1 != string2
 
 
 
 def corrector(reference, string, repetitions, coordinates, size, allowed): # correct for n-nucleotides insertions/deletions/substitutions inside a repetitive range
 
-    corr_=[]
+	corr_=[]
 
-    coords=modifier(coordinates)
+	coords=modifier(coordinates)
 
-    for reps in repetitions:
+	for reps in repetitions:
 
-        self_=list(look_for_self(reps,string))
-        self__= Get_Alignment_Coordinates(coords,self_)
+		self_=list(look_for_self(reps,string))
+		self__= Get_Alignment_Coordinates(coords,self_)
 
-        ranges=[]
+		ranges=[]
 
-        for i in range(len(self_)-1):
+		for i in range(len(self_)-1):
 
-            if self__[i+1][1]-self__[i][1] <= len(reps): #coords are subsequent or in inserted region
+			if self__[i+1][1]-self__[i][1] <= len(reps): #coords are subsequent or in inserted region
 
-                if check_edit(reps,string[self_[i][1]+len(reps):self_[i+1][1]], allowed): #reps are subsequent or separated by n-allowed chars or by a string with edit distance n from reps
+				if check_edit(reps,string[self_[i][1]+len(reps):self_[i+1][1]], allowed): #reps are subsequent or separated by n-allowed chars or by a string with edit distance n from reps
 
-                    ranges.append((self_[i][1],self_[i+1][1])) #accept as putative interval with reps
+					ranges.append((self_[i][1],self_[i+1][1])) #accept as putative interval with reps
 
-                else:
+				else:
 
-                    continue
+					continue
 
-            else: #coordinates are more distant than len reps
+			else: #coordinates are more distant than len reps
 
-                if check_edit(reps,string[self_[i][1]+len(reps):self_[i+1][1]], allowed) and check_ref(reference[self__[i][1]-1:self__[i+1][1]],string[self_[i][1]: self_[i+1][1]+1]):
+				if check_edit(reps,string[self_[i][1]+len(reps):self_[i+1][1]], allowed) and check_ref(reference[self__[i][1]-1:self__[i+1][1]],string[self_[i][1]: self_[i+1][1]+1]):
 
-                    ranges.append((self_[i][1],self_[i+1][1])) #allows same motifs separated by a string with edit distance n from the rep
+					ranges.append((self_[i][1],self_[i+1][1])) #allows same motifs separated by a string with edit distance n from the rep
 
-                else:
+				else:
 
-                    continue
-
-
-        collapsed_ranges= defaultdict(list)
-      
-        for x, y in ranges:
-
-            collapsed_ranges[x].append(y)
-            collapsed_ranges[y].append(x)
-
-        result = defaultdict(list)
-        visited = set()
-      
-        for vertex in collapsed_ranges:
-
-            if vertex not in visited:
-
-                dfs(collapsed_ranges, visited, vertex, result, vertex) #collapse ranges that have common bounds
+					continue
 
 
-        if len(result) != 0:
+		collapsed_ranges= defaultdict(list)
+	  
+		for x, y in ranges:
 
-            corr_.extend(Get_Alignment_Coordinates(coords,[(reps, interv[0], interv[-1]+(len(reps)-1)) for interv in result.values() if interv[-1]+len(reps)-interv[0] >= size]))
+			collapsed_ranges[x].append(y)
+			collapsed_ranges[y].append(x)
 
-        else:
+		result = defaultdict(list)
+		visited = set()
+	  
+		for vertex in collapsed_ranges:
 
-            continue
+			if vertex not in visited:
 
-    if corr_ == []: #nearly impossible
+				dfs(collapsed_ranges, visited, vertex, result, vertex) #collapse ranges that have common bounds
 
-        return corr_
 
-    s_corr_=sorted(corr_, key=itemgetter(1,2))
+		if len(result) != 0:
 
-    mod_int=nestover(s_corr_, string, coords)
+			corr_.extend(Get_Alignment_Coordinates(coords,[(reps, interv[0], interv[-1]+(len(reps)-1)) for interv in result.values() if interv[-1]+len(reps)-interv[0] >= size]))
 
-    return mod_int
+		else:
+
+			continue
+
+	if corr_ == []: #nearly impossible
+
+		return corr_
+
+	s_corr_=sorted(corr_, key=itemgetter(1,2))
+
+	mod_int=nestover(s_corr_, string, coords)
+
+	return mod_int
