f76483e303d8f5caec7dbc27e7eb5b01e21a1831
Davide Bolognini <davidebolognini7@gmail.com>
2019-09-03 11:45:36 +0200

    Update consensus.sh

--- TRiCoLOR/REFER/consensus.sh
+++ TRiCoLOR/REFER/consensus.sh
@@ -2,8 +2,8 @@
 
 cd $1
 
-$2 consensus -f fasta -t $3 -a $4".al.fa.gz" -c $4".cs.fa.gz" $5 && rm $5 && rm $5".fai"
-minimap2 -ax $6 $7 $4".cs.fa.gz" > $4".cs.sam" && rm $4".al.fa.gz" && rm $4".cs.fa.gz"
-samtools view -b $4".cs.sam" > $4".cs.bam" && rm $4".cs.sam"
-samtools sort $4".cs.bam" > $4".cs.srt.bam" && rm $4".cs.bam"
-samtools index $4".cs.srt.bam"
+$2 -1 1 5 -4 -8 -6 $4 > $3".cs.fa" && rm $4
+minimap2 -ax $5 $6 $3".cs.fa" > $3".cs.sam" && rm $3".cs.fa"
+samtools view -b $3".cs.sam"  > $3".cs.bam" && rm $3".cs.sam"
+samtools sort $3".cs.bam" > $3".cs.srt.bam" && rm $3".cs.bam"
+samtools index $3".cs.srt.bam"
