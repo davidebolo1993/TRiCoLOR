874238c8095754cba98da26e38fda5d8f80819b6
Davide Bolognini <davidebolognini7@gmail.com>
2019-09-03 11:28:52 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,9 +6,9 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -Dspoa_build_executable=ON ..
 make
 cd ..
-if [ -f ../consensus.cpp ]
+if [ -f ../TRiCoLOR/REFER/consensus.cpp ]
 then
-    mv ../consensus.cpp .
+    mv ../TRiCoLOR/REFER/consensus.cpp .
 fi
 g++ consensus.cpp -std=c++11 -Iinclude/ -Lbuild/lib/ -lspoa -o consensus -static
 cp consensus ../TRiCoLOR/REFER/
