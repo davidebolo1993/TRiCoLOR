9ff94dc825869e01cf417843ebc2f15c4d09a718
davidebolo1993 <davidebolognini7@gmail.com>
2019-09-06 18:10:54 +0200

    recommit

--- TRiCoLOR/REFER/consensus.cpp
+++ TRiCoLOR/REFER/consensus.cpp
@@ -2,53 +2,32 @@
 #include <iostream>
 #include <fstream>
 
-
 int main(int argc, char** argv) {
+
+    std::string infile = argv[6];
 
     std::vector<std::string> sequences = {};
     std::vector<std::string> headers = {};
-    std::string infile = argv[6];
 
     std::ifstream input(infile, std::ios_base::in | std::ios_base::binary);
     std::string line;
-    bool first_seq=true;
-
 
     if (input.is_open()) {
-        
 
         while (std::getline(input, line)) {
 
             if (line[0] == '>') {
                 headers.push_back(line);
-                continue;
             }
-
             else {
-
-
-                if (!first_seq) {
-                    
-                    sequences.push_back(line);
-                }
-
-            else {
-
-                    first_seq=false;
-                    sequences.push_back(line);
-                }                        
-
+                sequences.push_back(line);
             }
-
         }
-
+        
         input.close();
-
     }
 
-
     for (std::vector<std::string>::const_iterator i = headers.begin(); i != headers.end(); ++i) std::cerr << *i << '\n';
-
 
     auto alignment_engine = spoa::createAlignmentEngine(static_cast<spoa::AlignmentType>(atoi(argv[1])),
         atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
@@ -66,3 +45,4 @@ int main(int argc, char** argv) {
     fprintf(stdout, "%s\n", consensus.c_str());
 
 }
+
